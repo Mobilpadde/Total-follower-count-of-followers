7714379c2034c05301a8c791350ee858c1e48529
Mads Cordes <Mobilpadde@gmail.com>
2013-01-16 14:13:24 -0800

    Create count.php

---
+++ count.php
@@ -0,0 +1,12 @@
+<?
+  $me = simplexml_load_file("https://api.twitter.com/1/followers/ids.xml?screen_name=Mobilpadde");
+	echo "<pre>";
+	print_r($me->ids->id);
+	foreach($me->ids->id as $id){
+		$follower = simplexml_load_file("https://api.twitter.com/1/followers/ids.xml?user_id=".$id);
+		$total += count($follower->ids->id);
+		echo "$id got ".count($follower->ids->id)." followers<br />";
+	}
+	echo "That'll be $total in total";
+	echo "</pre>";
+?>
