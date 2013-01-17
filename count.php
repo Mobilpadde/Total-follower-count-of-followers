a07eacf43a708e7d9bd7902f95444761d96e0d03
Mads Cordes <Mobilpadde@gmail.com>
2013-01-17 15:15:31 +0100

    Private users are now included :D

--- count.php
+++ count.php
@@ -1,11 +1,16 @@
 <?
-	$me = simplexml_load_file("https://api.twitter.com/1/followers/ids.xml?screen_name=Mobilpadde");
+	$me = simplexml_load_file("https://api.twitter.com/1/followers/ids.xml?screen_name=Poiar");
 	echo "<pre>";
 	print_r($me->ids->id);
 	foreach($me->ids->id as $id){
-		$follower = simplexml_load_file("https://api.twitter.com/1/followers/ids.xml?user_id=".$id);
+		/** Won't work on private users...
+		$follower = simplexml_load_file("https://api.twitter.com/1/followers/ids.xml?user_id=$id");
 		$total += count($follower->ids->id);
 		echo "$id got ".count($follower->ids->id)." followers<br />";
+		**/
+		$follower = simplexml_load_file("https://api.twitter.com/1/users/show.xml?user_id=$id");
+		$total += $follower->followers_count;
+		echo "$id got ".$follower->followers_count." followers<br />";
 	}
 	echo "That'll be $total in total";
 	echo "</pre>";
